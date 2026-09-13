d8b5f8885ce836cf1462db05940bedb5cad3cae7
김찬영 <gimchan-yeong@gimchan-yeong-ui-MacBookAir.local>
2026-09-13 17:25:47 +0900

    Deploy native Safari PDF input

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1602082819" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4274056881" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
